08e21b200fe9fc5b90fb4084130966b092515fb0
Fenomen-Alex <alexfenomen@gmail.com>
2022-09-26 17:01:57 +0300

    Get movie by id

--- cmd/api/movieHandlers.go
+++ cmd/api/movieHandlers.go
@@ -3,10 +3,8 @@ package main
 import (
 	"errors"
 	"github.com/julienschmidt/httprouter"
-	"go-movies-backend/cmd/models"
 	"net/http"
 	"strconv"
-	"time"
 )
 
 func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
@@ -21,17 +19,19 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 
 	app.logger.Println("id is", id)
 
-	movie := models.Movie{
-		ID:          id,
-		Title:       "Some movie",
-		Description: "Some description",
-		Year:        2021,
-		ReleaseDate: time.Date(2021, 01, 01, 01, 0, 0, 0, time.Local),
-		Runtime:     100,
-		MPAARating:  "PG-13",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-	}
+	movie, err := app.models.DB.Get(id)
+
+	//movie := models.Movie{
+	//	ID:          id,
+	//	Title:       "Some movie",
+	//	Description: "Some description",
+	//	Year:        2021,
+	//	ReleaseDate: time.Date(2021, 01, 01, 01, 0, 0, 0, time.Local),
+	//	Runtime:     100,
+	//	MPAARating:  "PG-13",
+	//	CreatedAt:   time.Now(),
+	//	UpdatedAt:   time.Now(),
+	//}
 
 	err = app.writeJSON(w, http.StatusOK, movie, "movie")
 }
